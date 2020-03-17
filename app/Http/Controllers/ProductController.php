231b6960fd0d37705a0801a84f70239704145856
Mosab irwished <eng.mosabirwished@gmail.com>
2020-03-17 21:20:48 +0200

    Product update

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
+
+
+
     public function index()
     {
         return ProductCollection::collection(Product::paginate(5));
@@ -23,6 +32,35 @@ class ProductController extends Controller
     {
         return new ProductResource($product);
     }
+
+
+
+    public function store(ProductRequest $request)
+    {
+        $product = new Product();
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->user_id = auth()->user()->id;
+        $product->save();
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+    }
+
+
+    public function update(Request $request, Product $product)
+    {
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        return response([
+            'data' => new ProductResource($product)
+        ],Response::HTTP_CREATED);
+    }
+
 
 
 
