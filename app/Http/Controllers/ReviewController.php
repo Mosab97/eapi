5e974b51b5c073f2abe6263b8b1ecc7ef91b1b4e
Mosab irwished <eng.mosabirwished@gmail.com>
2020-03-17 23:29:36 +0200

    Review created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ReviewRequest;
 use App\Http\Resources\ReviewResource;
 use App\Product;
 use App\Review;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class ReviewController extends Controller
 {
@@ -13,4 +16,16 @@ class ReviewController extends Controller
     {
         return ReviewResource::collection($product->reviews);
     }
+
+
+
+    public function store(ReviewRequest $request,Product $product)
+    {
+        $review = new Review($request->all());
+        $product->reviews()->save($review);
+        return response([
+            'data' => new ReviewResource($review)
+        ],Response::HTTP_CREATED);
+    }
+
 }
