d17ed2a7e98c30eb5473a0ff034fc60e34454b5d
Mosab irwished <eng.mosabirwished@gmail.com>
2020-03-17 20:17:11 +0200

    Review Resource & Product Pagintation

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -2,16 +2,11 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Http\Resources\Json\JsonResource;
 
-class ReviewResource extends Resource
+class ReviewResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param  \Illuminate\Http\Request
-     * @return array
-     */
+
     public function toArray($request)
     {
         return [
